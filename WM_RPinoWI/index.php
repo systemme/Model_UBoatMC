6386c505adbcfe5bebc89c19ac6cbf6cba16935d
systemme <michele@systemme.net>
2016-07-26 15:56:28 +0200

    Update...
    Signed-off-by: systemme <michele@systemme.net>

--- WM_RPinoWI/index.php
+++ WM_RPinoWI/index.php
@@ -3,32 +3,32 @@
 	[WM_RPinoWI]
 	U-boat Module Control Web Interface
 
-    	Copyright (C) 2016 Martinelli Michele
+    Copyright (C) 2016 Martinelli Michele
 
    	This program is free software: you can redistribute it and/or modify
-    	it under the terms of the GNU General Public License as published by
-    	the Free Software Foundation, either version 3 of the License, or
-    	(at your option) any later version.
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
 
-    	This program is distributed in the hope that it will be useful,
-    	but WITHOUT ANY WARRANTY; without even the implied warranty of
-    	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    	GNU General Public License for more details.
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
 
-    	You should have received a copy of the GNU General Public License
-    	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 	/* Session */
 	define('_WMEX', 1);
 
-        /* Path Definitions */
-        define('WM_DS', DIRECTORY_SEPARATOR);	// Directory Separator
-        define('WM_BASE', dirname(__FILE__));	// Root Path
-        define('WM_INC', WM_BASE.'/includes');  // Includes Path
-        define('WM_SYS', WM_BASE.'/system');    // System Path
-        define('WM_ADM', WM_BASE.'/admin');     // Administrator Path
-        define('WM_OWI', WM_BASE.'/opwi'); 	// Operator WebInterface Path
+    /* Path Definitions */
+    define('WM_DS', DIRECTORY_SEPARATOR);	// Directory Separator
+    define('WM_BASE', dirname(__FILE__));	// Root Path
+    define('WM_INC', WM_BASE.'/includes');  // Includes Path
+    define('WM_SYS', WM_BASE.'/system');    // System Path
+    define('WM_ADM', WM_BASE.'/admin');     // Administrator Path
+    define('WM_OWI', WM_BASE.'/opwi'); 	// Operator WebInterface Path
 
 	/* Configuration */
 	require (WM_SYS . '/configuration.php');
